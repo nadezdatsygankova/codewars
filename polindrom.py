cf435c06923548c1ab10d634df87b4bb3b7b3aed
Nadezda <nadiapritchina@gmail.com>
2021-02-24 22:22:47 -0500

    init: add polindrom.py

---
+++ polindrom.py
@@ -0,0 +1,6 @@
+def is_palindrome(string):
+    string = str(string);
+    for i in range(0, int(len(string)/2)): 
+        if string[i] != string[len(string)-i-1]:
+            return False
+    return True
