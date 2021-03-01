743e23f5a5584959677f91a70c2088dd305632c1
Nadezda <>
2021-03-01 11:22:48 -0500

    Init: add anagramDetection

---
+++ anagramDetection.cpp
@@ -0,0 +1,43 @@
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <iterator>
+#include <cctype>
+bool isAnagram(std::string test, std::string original);
+using namespace std;
+
+int main()
+{
+   std::cout << isAnagram("dumble", "bumble");
+
+   return 0;
+}
+
+
+bool isAnagram(std::string test, std::string original) {
+   int sizeTest = test.length();
+   int sizeOriginal = original.length();
+
+   if (sizeTest == sizeOriginal)
+   {
+
+      for (int i = 0; i < sizeTest; i++)
+      {
+         test[i] = toupper(test[i]);
+         original[i] = toupper(original[i]);
+      }
+      std::sort(test.begin(), test.end());
+      std::sort(original.begin(), original.end());
+
+      if (test == original)
+      {
+         return true;
+      }
+      else
+         return false;
+   }
+   else
+      return false;
+
+}
