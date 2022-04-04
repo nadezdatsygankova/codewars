9be6666e44158ca1af192850731344a106eb3bad
Nadia Tsygankova <47821694+nadezdatsygankova@users.noreply.github.com>
2022-04-04 17:43:31 -0400

    Create Javascript_Mathematician.js

---
+++ Javascript_Mathematician.js
@@ -0,0 +1,23 @@
+/*
+You are writing a function that takes two sets of arguments of arbitrary length. The return value will be the sum of the values of all of the arguments.
+The function should contain at least 1 argument per set.
+calculate(1)(1) // should return 2
+calculate(1,1)(1) // should return 3
+calculate(1,1)(1,-1) // should return 2
+calculate(2,4)(3,7,1) // should return 17
+
+*/
+
+function calculate() {
+let first = 0;
+	for(let i=0; i<arguments.length; i++){
+		first += arguments[i]
+	}
+	return function(a) {
+		for(let j=0; j<arguments.length; j++){
+		first += arguments[j];
+	}
+	return first;
+	}	
+}
+
